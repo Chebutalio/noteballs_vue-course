1a480a02fc6467e4acfa498b9e18db78ae1548e4
chebutalio <chebutalio@gmail.com>
2024-04-04 11:17:50 +0300

    added functionality to add new note

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,4 +1,39 @@
 <script setup>
+  import { ref } from "vue";
+
+  const newNote = ref('');
+  const newNoteRef = ref(null);
+  const notes = ref([
+    {
+      id: 'id1',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic ipsum maxime' +
+          ' necessitatibus nemo nihil odit quasi, temporibus voluptas voluptatem!',
+    },
+    {
+      id: 'id2',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic ipsum maxime',
+    },
+    {
+      id: 'id3',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic ipsum maxime' +
+          ' necessitatibus nemo nihil odit quasi, temporibus voluptas voluptatem!' +
+          ' necessitatibus nemo nihil odit quasi, temporibus voluptas voluptatem!',
+    }
+  ]);
+
+  const addNewNote = () => {
+    let currentDate = new Date().getTime();
+    let id = currentDate.toString();
+
+    let note = {
+      id: id,
+      content: newNote.value,
+    }
+
+    notes.value.unshift(note);
+    newNote.value = '';
+    newNoteRef.value.focus();
+  }
 
 </script>
 
@@ -7,23 +42,36 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note"></textarea>
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          ></textarea>
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            class="button is-link has-background-success"
+            @click="addNewNote()"
+            :disabled="!newNote"
+          >
             Add Note
           </button>
         </div>
       </div>
     </div>
 
-    <div v-for="i in 3" class="card mb-4">
+    <div
+      v-for="note in notes"
+      :key="note.id"
+      class="card mb-4"
+    >
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea hic ipsum maxime necessitatibus nemo nihil odit quasi, temporibus voluptas voluptatem!
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
